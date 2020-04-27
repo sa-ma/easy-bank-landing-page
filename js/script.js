22c302565cf4e3063abae03fc15c44db74d52220
Samaila Bala <samailabalap@gmail.com>
2020-04-27 18:44:42 +0100

    feat(header): add header
    - add header layout
    - add button component
    - add responsive menu

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const navbarToggle = document.querySelector('.responsive-menu');
+const navbar = document.querySelector('.header__menu__navigation');
+
+navbarToggle.addEventListener('click', () => {
+  if (navbar.style.display === 'none' || navbar.style.display === '') {
+    document.querySelector('main').style.backgroundColor = 'hsl(233, 8%, 62%)';
+    document
+      .querySelector('.responsive-menu img')
+      .setAttribute('src', 'images/icon-close.svg');
+    navbar.style.display = 'flex';
+  } else {
+    document
+      .querySelector('.responsive-menu img')
+      .setAttribute('src', 'images/icon-hamburger.svg');
+    navbar.style.display = 'none';
+  }
+});
